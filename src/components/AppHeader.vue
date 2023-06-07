4e6df89fa757c463acd9683a74415694e229d1f4
MatteoGunetto <Matteogunetto@tiscali.it>
2023-06-07 14:43:51 +0200

    milestone 0

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+import SearchForm from "./AppSearch.vue";
+    export default{
+        name: 'AppHeader',
+        components: {
+        SearchForm
+        }
+    }
+</script>
+
+<template>
+    <h1>Header</h1>
+    <SearchForm />
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,13 @@
+<script>
+    
+    export default{
+        name: 'SarchForm'
+    }
+</script>
+
+<template>
+    <h1>SarchForm</h1>
+</template>
+
+<style lang="scss">
+</style>
